1ee07443ee0256cbec7a946ca3530100add6253e
Ayman Zahran <ah.zahran@outlook.com>
2021-04-01 20:43:54 +0200

    Adding Pipeline CLI

--- run_azure.sh
+++ run_azure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 az group create --resource-group $FLASK_RESOURCE_GROUP --location $AZURE_LOCATION --tags webserver-env="Production"
 az webapp up -g $FLASK_RESOURCE_GROUP -n $FLASK_WEB_APP
-az pipelines create --name "Contoso.CI" --organization https://dev.azure.com/aymancloudtesting7/Azure_CI-CD_MachineLearningApp --project Azure_CI-CD_MachineLearningApp --repository https://github.com/AymanZahran/Azure_CI-CD_MachineLearningApp
+az pipelines create --name $PIPELINE_NAME --description "Azure_CI-CD_MachineLearningApp" --organization $ORGANIZATION_NAME --project $PROJECT_NAME --repository $REPO_NAME --branch master --repository-type github --yml-path azure-pipelines.yml
