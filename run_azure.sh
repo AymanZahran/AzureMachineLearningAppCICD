c1d81c225c172258f80db6b21dff5dfae2ef871b
Ayman Zahran <ah.zahran@outlook.com>
2021-04-01 21:33:22 +0200

    configuring parrallel jobs

--- run_azure.sh
+++ run_azure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 az group create --resource-group $FLASK_RESOURCE_GROUP --location $AZURE_LOCATION --tags webserver-env="Production"
 az webapp up -g $FLASK_RESOURCE_GROUP -n $FLASK_WEB_APP
-az pipelines create --name $PIPELINE_NAME --description "Azure_CI-CD_MachineLearningApp" --organization $ORGANIZATION_NAME --project $PROJECT_NAME --repository $REPO_NAME --branch master --repository-type github --yml-path azure-pipelines.yml
+az pipelines create --name $PIPELINE_NAME --description $PIPELINE_DESCRIPTION --organization $ORGANIZATION_NAME --project $PROJECT_NAME --repository $REPO_NAME --branch master --repository-type github --yml-path azure-pipelines.yml --service-connection $SERVICE_CONNECTION
