e342b94065dfa5100b902f51e31fb282cceb9e58
Ayman Zahran <ah.zahran@outlook.com>
2021-04-02 10:03:10 +0200

    Adding Variable Group Automation

--- run_azure.sh
+++ run_azure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 az group create --resource-group $FLASK_RESOURCE_GROUP --location $AZURE_LOCATION --tags webserver-env="Production"
 az webapp up -g $FLASK_RESOURCE_GROUP -n $FLASK_WEB_APP
+az pipelines variable-group create --name Variable_Group --authorize true --organization $ORGANIZATION_NAME --project $PROJECT_NAME --variables azureServiceConnectionId=$AZURE_SERVICE_CONNECTION_ID webAppName=$FLASK_WEB_APP vmImageName=$AGENT_VM_IMAGE_NAME environmentName=$ENVIRONMENT_NAME  pythonVersion=$PYTHON_VERSION
 az pipelines create --name $PIPELINE_NAME --description $PIPELINE_DESCRIPTION --organization $ORGANIZATION_NAME --project $PROJECT_NAME --repository $REPO_NAME --branch master --repository-type github --yml-path azure-pipelines.yml 
-#az pipelines variable-group create --name ${FLASK_WEB_APP}_Variables --authorize true --organization $ORGANIZATION_NAME --project $PROJECT_NAME --variables webAppName=$FLASK_WEB_APP vmImageName='ubuntu-latest' environmentName='flask-sklearn' projectRoot=$(System.DefaultWorkingDirectory) pythonVersion='3.7'
