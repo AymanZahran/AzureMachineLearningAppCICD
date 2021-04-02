4febc4ed1ab8afcb9a1113a474c3a5da95f4e195
Ayman Zahran <ah.zahran@outlook.com>
2021-04-02 20:08:28 +0200

    Demo Commit

--- run_azure.sh
+++ run_azure.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#Create Resource Group
+echo "Create Resource Group"
 az group create --resource-group $FLASK_RESOURCE_GROUP --location $AZURE_LOCATION --tags webserver-env="Production"
 
 
-#Create Service Plan and Web App
+echo "Create Service Plan and Web App"
 az webapp up -g $FLASK_RESOURCE_GROUP -n $FLASK_WEB_APP
 
 
-#Create Variable Group
+echo "Create Variable Group"
 az pipelines variable-group create --name Variable_Group --authorize true --organization $ORGANIZATION_NAME --project $PROJECT_NAME --variables azureServiceConnectionId=$AZURE_SERVICE_CONNECTION_ID webAppName=$FLASK_WEB_APP vmImageName=$AGENT_VM_IMAGE_NAME environmentName=$ENVIRONMENT_NAME  pythonVersion=$PYTHON_VERSION
 
 
-#Create Azure Pipeline
+echo "Create Azure Pipeline"
 az pipelines create --name $PIPELINE_NAME --description $PIPELINE_DESCRIPTION --organization $ORGANIZATION_NAME --project $PROJECT_NAME --repository $REPO_NAME --branch master --repository-type github --yml-path azure-pipelines.yml 
